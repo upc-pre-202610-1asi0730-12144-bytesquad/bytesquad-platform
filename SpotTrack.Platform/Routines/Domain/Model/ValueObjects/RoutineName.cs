2f14d8668fb48b89299d74160a06e97bf342b81a
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-13 08:08:28 -0500

    feat(routines): new value objects

---
+++ SpotTrack.Platform/Routines/Domain/Model/ValueObjects/RoutineName.cs
@@ -0,0 +1,17 @@
+namespace SpotTrack.Platform.Routines.Domain.Model.ValueObjects;
+
+public record RoutineName
+{
+    public string Value { get; init; }
+    
+    public RoutineName() : this(string.Empty) { }
+
+    public RoutineName(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException("RoutineName cannot be null or whitespace.", nameof(value));
+        if (value.Length > 100)
+            throw new ArgumentException("RoutineName cannot be longer than 100 characters.", nameof(value));
+        Value = value;
+    }
+}
