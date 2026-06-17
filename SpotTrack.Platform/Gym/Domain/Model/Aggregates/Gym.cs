aafdc1257279406a497ee59d6891ec35eb6104c0
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-17 08:24:04 -0500

    feat(gym): add Branches collection and AddBranch method to Gym aggregate

--- SpotTrack.Platform/Gym/Domain/Model/Aggregates/Gym.cs
+++ SpotTrack.Platform/Gym/Domain/Model/Aggregates/Gym.cs
@@ -1,4 +1,5 @@
 using SpotTrack.Platform.Gyms.Domain.Model.Commands;
+using SpotTrack.Platform.Gyms.Domain.Model.Entities;
 using SpotTrack.Platform.Gyms.Domain.Model.ValueObjects;
 
 namespace SpotTrack.Platform.Gyms.Domain.Model.Aggregates;
@@ -11,11 +12,21 @@ public partial class Gym
 
     public Address Address { get; private set; } = null!;
 
+    private List<Branch> _branches = new();
+    public IReadOnlyCollection<Branch> Branches => _branches.AsReadOnly();
+
     private Gym() { }
 
     public Gym(CreateGymCommand command)
     {
         Name = new GymName(command.Name);
         Address = new Address(command.Street, command.District, command.City);
+    }
+
+    public void AddBranch(string name, string street, string district, string city)
+    {
+        var branchName = new BranchName(name);
+        var address = new Address(street, district, city);
+        _branches.Add(new Branch(branchName, address));
     }
 }
