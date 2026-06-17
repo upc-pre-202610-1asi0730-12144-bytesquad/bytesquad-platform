863db26df32efd8619e7f9279a1ec06f3b5336d1
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-17 04:59:55 -0500

    feat(profiles): use null-conditional in ClientResourceFromEntityAssembler for nullable owned types

--- SpotTrack.Platform/Profiles/Interfaces/Rest/Transform/ClientResourceFromEntityAssembler.cs
+++ SpotTrack.Platform/Profiles/Interfaces/Rest/Transform/ClientResourceFromEntityAssembler.cs
@@ -6,6 +6,6 @@ namespace SpotTrack.Platform.Profiles.Interfaces.Rest.Transform;
 public static class ClientResourceFromEntityAssembler
 {
     public static ClientResource ToResourceFromEntity(Client client) =>
-        new(client.Id, client.UserId, client.FullName, client.Email.Address,
-            client.Phone.Number, client.Dni.Value);
+        new(client.Id, client.UserId, client.FullName, client.Email?.Address ?? string.Empty,
+            client.Phone?.Number ?? string.Empty, client.Dni?.Value ?? string.Empty);
 }
