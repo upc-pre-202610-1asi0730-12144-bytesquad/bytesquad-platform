298184a0509f742c612a8a3bbed03280516a9c03
Carlsss28 <carlagmalejandra28@gmail.com>
2026-06-17 09:04:38 -0500

    feat(membership): add IMembershipCommandService interface

---
+++ SpotTrack.Platform/Membership/Application/CommandServices/IMembershipCommandService.cs
@@ -0,0 +1,10 @@
+using SpotTrack.Platform.Memberships.Domain.Model.Aggregates;
+using SpotTrack.Platform.Memberships.Domain.Model.Commands;
+using SpotTrack.Platform.Shared.Application.Model;
+
+namespace SpotTrack.Platform.Memberships.Application.CommandServices;
+
+public interface IMembershipCommandService
+{
+    Task<Result<Membership>> Handle(CreateActivateMembershipCommand command, CancellationToken cancellationToken);
+}
