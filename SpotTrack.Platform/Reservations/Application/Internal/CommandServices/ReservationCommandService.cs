d58c1957651265ac0a126ed74ea3584f9d6c696b
Carlsss28 <carlagmalejandra28@gmail.com>
2026-06-17 07:14:55 -0500

    feat(reservation):add end reservation command services

--- SpotTrack.Platform/Reservations/Application/Internal/CommandServices/ReservationCommandService.cs
+++ SpotTrack.Platform/Reservations/Application/Internal/CommandServices/ReservationCommandService.cs
@@ -141,6 +141,38 @@ public class ReservationCommandService(
             cancellationToken);
         return Result<Reservation>.Success(reservation);
     }
+
+    public async Task<Result<Reservation>> Handle(
+        CreateEndReservationCommand command, CancellationToken cancellationToken)
+    {
+       var reservation = await reservationRepository.FindByIdAsync(command.ReservationId,
+           cancellationToken);
+       if (reservation is null)
+           return Result<Reservation>.Failure(
+               ReservationsError.ReservationNotFound,
+               localizer[nameof(ReservationsError.ReservationNotFound)]);
+        try
+        {
+            reservation.End();
+        }
+        catch (InvalidOperationException ex)
+        {
+            return Result<Reservation>.Failure(
+                ReservationsError.InvalidReservationStatus,
+                ex.Message);
+        }
+        try
+        {
+            await unitOfWork.CompleteAsync(cancellationToken);
+            return Result<Reservation>.Success(reservation);
+        }
+        catch (DbUpdateException)
+        {
+            return Result<Reservation>.Failure(
+                ReservationsError.DatabaseError,
+                localizer[nameof(ReservationsError.DatabaseError)]);
+        }
+    }
 }
 
 
