00209c40f1a0fdc61239fae3280978337b40203e
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-16 21:17:13 -0500

    feat(routine): implement RoutineQueryService

--- SpotTrack.Platform/Routines/Application/Internal/QueryServices/RoutineQueryService.cs
+++ SpotTrack.Platform/Routines/Application/Internal/QueryServices/RoutineQueryService.cs
@@ -1,6 +1,15 @@
+using SpotTrack.Platform.Routines.Application.QueryServices;
+using SpotTrack.Platform.Routines.Domain.Model.Aggregates;
+using SpotTrack.Platform.Routines.Domain.Model.Queries;
+using SpotTrack.Platform.Routines.Domain.Repositories;
+
 namespace SpotTrack.Platform.Routines.Application.Internal.QueryServices;
 
-public class RoutineQueryService
+public class RoutineQueryService(IRoutineRepository routineRepository) : IRoutineQueryService
 {
-    
+    public async Task<Routine?> Handle(GetRoutineByIdQuery query, CancellationToken cancellationToken)
+        => await routineRepository.FindByIdAsync(query.RoutineId, cancellationToken);
+
+    public async Task<IEnumerable<Routine>> Handle(GetAllRoutinesByClientIdQuery query, CancellationToken cancellationToken)
+        => await routineRepository.FindAllByClientIdAsync(query.ClientId, cancellationToken);
 }
