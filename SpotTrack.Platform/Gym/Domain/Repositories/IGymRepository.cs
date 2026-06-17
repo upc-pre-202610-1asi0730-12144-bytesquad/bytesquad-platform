78eb98b03116bbc0cca7516ce81a27b60a7e91fa
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-17 07:41:15 -0500

    feat(gym): add IGymRepository interface

---
+++ SpotTrack.Platform/Gym/Domain/Repositories/IGymRepository.cs
@@ -0,0 +1,8 @@
+using SpotTrack.Platform.Gym.Domain.Model.Aggregates;
+using SpotTrack.Platform.Shared.Domain.Repositories;
+
+namespace SpotTrack.Platform.Gym.Domain.Repositories;
+
+public interface IGymRepository : IBaseRepository<Gym>
+{
+}
