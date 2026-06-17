abbb34fabd2d8179efc2aff3d61e6e0086c1e4f4
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-17 08:24:27 -0500

    feat(gym): implement Handle(CreateBranchCommand) in GymCommandService

--- SpotTrack.Platform/Gym/Application/Internal/CommandServices/GymCommandService.cs
+++ SpotTrack.Platform/Gym/Application/Internal/CommandServices/GymCommandService.cs
@@ -1,4 +1,5 @@
 using SpotTrack.Platform.Gyms.Domain.Model.Aggregates;
+using SpotTrack.Platform.Gyms.Domain.Model.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
 using SpotTrack.Platform.Gyms.Domain.Model;
@@ -52,6 +53,51 @@ public class GymCommandService(
         catch (Exception)
         {
             return Result<Gym>.Failure(
+                GymError.InternalServerError,
+                localizer[nameof(GymError.InternalServerError)]);
+        }
+    }
+
+    public async Task<Result<Branch>> Handle(CreateBranchCommand command, CancellationToken cancellationToken)
+    {
+        var gym = await gymRepository.FindByIdAsync(command.GymId, cancellationToken);
+        if (gym is null)
+            return Result<Branch>.Failure(
+                GymError.GymNotFound,
+                localizer[nameof(GymError.GymNotFound)]);
+
+        try
+        {
+            gym.AddBranch(command.Name, command.Street, command.District, command.City);
+        }
+        catch (ArgumentException)
+        {
+            return Result<Branch>.Failure(
+                GymError.InvalidData,
+                localizer[nameof(GymError.InvalidData)]);
+        }
+
+        try
+        {
+            gymRepository.Update(gym);
+            await unitOfWork.CompleteAsync(cancellationToken);
+            return Result<Branch>.Success(gym.Branches.Last());
+        }
+        catch (OperationCanceledException)
+        {
+            return Result<Branch>.Failure(
+                GymError.OperationCancelled,
+                localizer[nameof(GymError.OperationCancelled)]);
+        }
+        catch (DbUpdateException)
+        {
+            return Result<Branch>.Failure(
+                GymError.DatabaseError,
+                localizer[nameof(GymError.DatabaseError)]);
+        }
+        catch (Exception)
+        {
+            return Result<Branch>.Failure(
                 GymError.InternalServerError,
                 localizer[nameof(GymError.InternalServerError)]);
         }
