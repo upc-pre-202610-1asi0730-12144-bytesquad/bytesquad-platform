f597ac56a45c57bc66e99f4fbd900b1650a0da8d
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-17 08:24:11 -0500

    feat(gym): add Handle(CreateBranchCommand) to IGymCommandService

--- SpotTrack.Platform/Gym/Domain/Services/IGymCommandService.cs
+++ SpotTrack.Platform/Gym/Domain/Services/IGymCommandService.cs
@@ -1,5 +1,6 @@
 using SpotTrack.Platform.Gyms.Domain.Model.Aggregates;
 using SpotTrack.Platform.Gyms.Domain.Model.Commands;
+using SpotTrack.Platform.Gyms.Domain.Model.Entities;
 using SpotTrack.Platform.Shared.Application.Model;
 
 namespace SpotTrack.Platform.Gyms.Domain.Services;
@@ -7,4 +8,5 @@ namespace SpotTrack.Platform.Gyms.Domain.Services;
 public interface IGymCommandService
 {
     Task<Result<Gym>> Handle(CreateGymCommand command, CancellationToken cancellationToken);
+    Task<Result<Branch>> Handle(CreateBranchCommand command, CancellationToken cancellationToken);
 }
