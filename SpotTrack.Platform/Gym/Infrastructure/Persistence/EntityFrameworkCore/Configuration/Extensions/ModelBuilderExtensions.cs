0fbc53bdb1b8056ec565f86962f2b95ef728e8d7
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-17 08:24:54 -0500

    feat(gym): add Branch entity mapping with HasMany and owned type VOs in EF Core config

--- SpotTrack.Platform/Gym/Infrastructure/Persistence/EntityFrameworkCore/Configuration/Extensions/ModelBuilderExtensions.cs
+++ SpotTrack.Platform/Gym/Infrastructure/Persistence/EntityFrameworkCore/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -1,4 +1,5 @@
 using SpotTrack.Platform.Gyms.Domain.Model.Aggregates;
+using SpotTrack.Platform.Gyms.Domain.Model.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace SpotTrack.Platform.Gyms.Infrastructure.Persistence.EntityFrameworkCore.Configuration.Extensions;
@@ -19,6 +20,32 @@ public static class ModelBuilderExtensions
             });
 
             entity.OwnsOne(g => g.Address, address =>
+            {
+                address.WithOwner().HasForeignKey("Id");
+                address.Property(a => a.Street).IsRequired().HasMaxLength(200).HasColumnName("street");
+                address.Property(a => a.District).IsRequired().HasMaxLength(100).HasColumnName("district");
+                address.Property(a => a.City).IsRequired().HasMaxLength(100).HasColumnName("city");
+            });
+
+            entity.HasMany(g => g.Branches)
+                .WithOne()
+                .HasForeignKey("gym_id")
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        builder.Entity<Branch>(entity =>
+        {
+            entity.HasKey(b => b.Id);
+            entity.Property(b => b.Id).ValueGeneratedOnAdd();
+            entity.ToTable("branches");
+
+            entity.OwnsOne(b => b.Name, name =>
+            {
+                name.WithOwner().HasForeignKey("Id");
+                name.Property(n => n.Value).IsRequired().HasMaxLength(100).HasColumnName("name");
+            });
+
+            entity.OwnsOne(b => b.Address, address =>
             {
                 address.WithOwner().HasForeignKey("Id");
                 address.Property(a => a.Street).IsRequired().HasMaxLength(200).HasColumnName("street");
