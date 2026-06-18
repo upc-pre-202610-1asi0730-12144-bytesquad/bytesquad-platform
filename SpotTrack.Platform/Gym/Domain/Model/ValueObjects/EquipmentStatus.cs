509f5658ed21717cfe8334e6c3fa9fbe024d5119
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-06-17 21:33:44 -0500

    feat(gyms): add EquipmentStatus enum

---
+++ SpotTrack.Platform/Gym/Domain/Model/ValueObjects/EquipmentStatus.cs
@@ -0,0 +1,11 @@
+namespace SpotTrack.Platform.Gyms.Domain.Model.ValueObjects;
+
+public enum EquipmentStatus
+{
+    Available,
+    OutOfService,
+    Maintenance,
+    Active,
+    Occupied,
+    Decommissioned
+}
