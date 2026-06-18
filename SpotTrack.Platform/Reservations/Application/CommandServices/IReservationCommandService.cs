458752d27bed30964703cd7da4c027699d523efd
Carlsss28 <carlagmalejandra28@gmail.com>
2026-06-18 16:12:00 -0500

    feat(reservations): declare Handle for CreateRequestEquipmentStatusChangeToAvailableCommand in IReservationCommandService

--- SpotTrack.Platform/Reservations/Application/CommandServices/IReservationCommandService.cs
+++ SpotTrack.Platform/Reservations/Application/CommandServices/IReservationCommandService.cs
@@ -13,4 +13,5 @@ public interface IReservationCommandService
     Task<Result<Reservation>>Handle(CreateSubmitRequestOccupyEquipmentCommand command, CancellationToken cancellationToken);
     Task<Result<Reservation>>Handle(CreateEndReservationCommand command, CancellationToken cancellationToken);
     Task<Result<Reservation>> Handle(CreateStartReservationTimerCommand command, CancellationToken cancellationToken);
+    Task<Result<Reservation>> Handle(CreateRequestEquipmentStatusChangeToAvailableCommand command, CancellationToken cancellationToken);
 }
