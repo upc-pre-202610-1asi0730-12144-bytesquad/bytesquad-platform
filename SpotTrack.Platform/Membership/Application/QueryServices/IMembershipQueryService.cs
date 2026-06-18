0b5a08bf1db2502bbfba836802493985fb66b2a3
Carlsss28 <carlagmalejandra28@gmail.com>
2026-06-17 19:22:56 -0500

    feat(membership): add GetAllMembershipsByClientId to IMembershipQueryService

--- SpotTrack.Platform/Membership/Application/QueryServices/IMembershipQueryService.cs
+++ SpotTrack.Platform/Membership/Application/QueryServices/IMembershipQueryService.cs
@@ -1,5 +1,9 @@
+using SpotTrack.Platform.Memberships.Domain.Model.Aggregates;
+using SpotTrack.Platform.Memberships.Domain.Model.Queries;
+
 namespace SpotTrack.Platform.Memberships.Application.QueryServices;
 
 public interface IMembershipQueryService
 {
+    Task<IEnumerable<Membership>> Handle(GetAllMembershipsByClientIdQuery query, CancellationToken cancellationToken);
 }
