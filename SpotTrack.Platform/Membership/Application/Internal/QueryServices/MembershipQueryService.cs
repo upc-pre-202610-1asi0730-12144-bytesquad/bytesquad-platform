d44f477593d0b13fa9894efacfcef7e080a2ce44
Carlsss28 <carlagmalejandra28@gmail.com>
2026-06-17 19:23:05 -0500

    feat(membership): implement GetAllMembershipsByClientId in MembershipQueryService

--- SpotTrack.Platform/Membership/Application/Internal/QueryServices/MembershipQueryService.cs
+++ SpotTrack.Platform/Membership/Application/Internal/QueryServices/MembershipQueryService.cs
@@ -1,4 +1,6 @@
 using SpotTrack.Platform.Memberships.Application.QueryServices;
+using SpotTrack.Platform.Memberships.Domain.Model.Aggregates;
+using SpotTrack.Platform.Memberships.Domain.Model.Queries;
 using SpotTrack.Platform.Memberships.Domain.Repositories;
 
 namespace SpotTrack.Platform.Memberships.Application.Internal.QueryServices;
@@ -6,4 +8,8 @@ namespace SpotTrack.Platform.Memberships.Application.Internal.QueryServices;
 public class MembershipQueryService(IMembershipRepository membershipRepository)
     : IMembershipQueryService
 {
+    public async Task<IEnumerable<Membership>> Handle(
+        GetAllMembershipsByClientIdQuery query,
+        CancellationToken cancellationToken)
+        => await membershipRepository.FindAllByClientIdAsync(query.ClientId, cancellationToken);
 }
