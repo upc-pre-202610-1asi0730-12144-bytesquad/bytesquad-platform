b96e0379bd8a0e26971bf4cccb417ba6f617fea3
Carlsss28 <carlagmalejandra28@gmail.com>
2026-06-19 13:09:49 -0500

    feat(maintenance): add RequestMaintenanceCommandFromResourceAssembler

---
+++ SpotTrack.Platform/Maintenance/Interfaces/Rest/Transform/RequestMaintenanceCommandFromResourceAssembler.cs
@@ -0,0 +1,10 @@
+using SpotTrack.Platform.Maintenances.Domain.Model.Commands;
+using SpotTrack.Platform.Maintenances.Interfaces.Rest.Resources;
+
+namespace SpotTrack.Platform.Maintenances.Interfaces.Rest.Transform;
+
+public static class RequestMaintenanceCommandFromResourceAssembler
+{
+    public static CreateRequestMaintenanceCommand ToCommandFromResource(RequestMaintenanceResource resource) =>
+        new(resource.EquipmentId, resource.RequestedByAdminId, resource.Reason);
+}
