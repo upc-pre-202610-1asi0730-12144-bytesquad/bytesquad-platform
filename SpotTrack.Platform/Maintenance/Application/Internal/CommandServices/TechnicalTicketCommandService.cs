d78156a5f1e0bfe45ca34fea1471317e1616c3fc
Carlsss28 <carlagmalejandra28@gmail.com>
2026-06-19 13:36:30 -0500

    feat(maintenance): add TechnicalTicketCommandService implementing CreateTechnicalTicket

---
+++ SpotTrack.Platform/Maintenance/Application/Internal/CommandServices/TechnicalTicketCommandService.cs
@@ -0,0 +1,84 @@
+using Cortex.Mediator;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using SpotTrack.Platform.Gyms.Interfaces.Acl;
+using SpotTrack.Platform.Maintenances.Application.CommandServices;
+using SpotTrack.Platform.Maintenances.Domain.Model;
+using SpotTrack.Platform.Maintenances.Domain.Model.Aggregates;
+using SpotTrack.Platform.Maintenances.Domain.Model.Commands;
+using SpotTrack.Platform.Maintenances.Domain.Model.Events;
+using SpotTrack.Platform.Maintenances.Domain.Repositories;
+using SpotTrack.Platform.Maintenances.Resources;
+using SpotTrack.Platform.Shared.Application.Model;
+using SpotTrack.Platform.Shared.Domain.Repositories;
+
+namespace SpotTrack.Platform.Maintenances.Application.Internal.CommandServices;
+
+public class TechnicalTicketCommandService(
+    ITechnicalTicketRepository technicalTicketRepository,
+    IMaintenanceRepository maintenanceRepository,
+    IUnitOfWork unitOfWork,
+    IMediator mediator,
+    IStringLocalizer<MaintenanceMessages> localizer,
+    IGymContextFacade gymContextFacade)
+    : ITechnicalTicketCommandService
+{
+    public async Task<Result<TechnicalTicket>> Handle(
+        CreateTechnicalTicketCommand command,
+        CancellationToken cancellationToken)
+    {
+        var maintenance = await maintenanceRepository.FindByIdAsync(command.MaintenanceId, cancellationToken);
+        if (maintenance is null)
+            return Result<TechnicalTicket>.Failure(
+                TechnicalTicketError.MaintenanceNotFound,
+                localizer[nameof(TechnicalTicketError.MaintenanceNotFound)]);
+
+        TechnicalTicket ticket;
+        try
+        {
+            ticket = new TechnicalTicket(command);
+        }
+        catch (ArgumentException)
+        {
+            return Result<TechnicalTicket>.Failure(
+                TechnicalTicketError.InternalServerError,
+                localizer[nameof(TechnicalTicketError.InternalServerError)]);
+        }
+
+        try
+        {
+            await technicalTicketRepository.AddAsync(ticket, cancellationToken);
+            await unitOfWork.CompleteAsync(cancellationToken);
+        }
+        catch (OperationCanceledException)
+        {
+            return Result<TechnicalTicket>.Failure(
+                TechnicalTicketError.OperationCancelled,
+                localizer[nameof(TechnicalTicketError.OperationCancelled)]);
+        }
+        catch (DbUpdateException)
+        {
+            return Result<TechnicalTicket>.Failure(
+                TechnicalTicketError.DatabaseError,
+                localizer[nameof(TechnicalTicketError.DatabaseError)]);
+        }
+        catch (Exception)
+        {
+            return Result<TechnicalTicket>.Failure(
+                TechnicalTicketError.InternalServerError,
+                localizer[nameof(TechnicalTicketError.InternalServerError)]);
+        }
+
+        var marked = await gymContextFacade.MarkEquipmentOutOfServiceAsync(command.EquipmentId);
+        if (!marked)
+            return Result<TechnicalTicket>.Failure(
+                TechnicalTicketError.EquipmentUpdateFailed,
+                localizer[nameof(TechnicalTicketError.EquipmentUpdateFailed)]);
+
+        await mediator.PublishAsync(
+            TechnicalTicketCreatedEvent.FromTechnicalTicket(ticket),
+            cancellationToken);
+
+        return Result<TechnicalTicket>.Success(ticket);
+    }
+}
