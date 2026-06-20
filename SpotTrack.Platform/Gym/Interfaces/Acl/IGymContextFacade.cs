d14c7ad0f8bb2d2d3a422389bb39d61dae8cc043
Carlsss28 <carlagmalejandra28@gmail.com>
2026-06-19 20:11:24 -0500

    feat(gyms): add MarkEquipmentAvailableAsync to IGymContextFacade

--- SpotTrack.Platform/Gym/Interfaces/Acl/IGymContextFacade.cs
+++ SpotTrack.Platform/Gym/Interfaces/Acl/IGymContextFacade.cs
@@ -5,4 +5,5 @@ public interface IGymContextFacade
     Task<bool> OccupyEquipmentAsync(int equipmentId);
     Task<bool> ReleaseEquipmentAsync(int equipmentId);
     Task<bool> MarkEquipmentOutOfServiceAsync(int equipmentId);
+    Task<bool> MarkEquipmentAvailableAsync(int equipmentId);
 }
