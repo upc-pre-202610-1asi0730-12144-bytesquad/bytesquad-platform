f3316606b2895799e34ec61dfc6e61da57c37348
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-06 02:32:44 -0500

    fix(analytics): replace Random ID with InitializeId() from EF PK in ActivityReport

--- SpotTrack.Platform/Analytics/Domain/Model/Aggregates/ActivityReport.cs
+++ SpotTrack.Platform/Analytics/Domain/Model/Aggregates/ActivityReport.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using SpotTrack.Platform.Analytics.Domain.Model.Commands;
+﻿using SpotTrack.Platform.Analytics.Domain.Model.Commands;
 using SpotTrack.Platform.Analytics.Domain.Model.ValueObjects;
 
 namespace SpotTrack.Platform.Analytics.Domain.Model.Aggregates
@@ -7,20 +6,21 @@ namespace SpotTrack.Platform.Analytics.Domain.Model.Aggregates
     public class ActivityReport
     {
         public long Id { get; private set; }
-        public ActivityReportId ActivityReportId { get; private set; }
+        public ActivityReportId ActivityReportId { get; private set; } = null!;
         public long TotalUsageTime { get; private set; }
         public long DowntimeCost { get; private set; }
         public double PercentageComparison { get; private set; }
 
         protected ActivityReport() { }
 
-        public ActivityReport(RequestActivityAnalysisCommand command) 
+        public ActivityReport(RequestActivityAnalysisCommand command)
         {
-            ActivityReportId = new ActivityReportId(new Random().Next(1000, 100000));
             TotalUsageTime = command.TotalUsageTime;
             DowntimeCost = command.DowntimeCost;
             PercentageComparison = command.PercentageComparison;
         }
+
+        public void InitializeId() => ActivityReportId = new ActivityReportId(Id);
 
         public void UpdateTotalUsageTime(long totalUsageTime) 
         {
