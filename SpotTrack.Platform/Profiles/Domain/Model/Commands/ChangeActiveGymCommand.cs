e3acafa02190bbffb657716ed6e7c7a3728f1cfc
ImaLi06 <imanollimache@gmail.com>
2026-07-05 20:33:15 -0500

    feat(profiles): add client-gym association domain model

---
+++ SpotTrack.Platform/Profiles/Domain/Model/Commands/ChangeActiveGymCommand.cs
@@ -0,0 +1,3 @@
+namespace SpotTrack.Platform.Profiles.Domain.Model.Commands;
+
+public record ChangeActiveGymCommand(int ClientId, int GymId);
--- SpotTrack.Platform/Profiles/Domain/Model/ProfilesError.cs
+++ SpotTrack.Platform/Profiles/Domain/Model/ProfilesError.cs
@@ -8,5 +8,8 @@ public enum ProfilesError
     InvalidProfileData,
     OperationCancelled,
     DatabaseError,
-    InternalServerError
+    InternalServerError,
+    ProfileIncomplete,
+    AlreadyAssociatedWithGym,
+    GymAssociationNotFound
 }
