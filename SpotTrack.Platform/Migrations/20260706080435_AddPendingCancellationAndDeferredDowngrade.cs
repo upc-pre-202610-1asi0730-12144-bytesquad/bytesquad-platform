1b75cb2d3a56e21ea61f38e1a666d98df92b7d27
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-06 03:04:51 -0500

    feat(membership): add migration AddPendingCancellationAndDeferredDowngrade

---
+++ SpotTrack.Platform/Migrations/20260706080435_AddPendingCancellationAndDeferredDowngrade.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpotTrack.Platform.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPendingCancellationAndDeferredDowngrade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "pending_downgrade_plan",
+                table: "memberships",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "pending_downgrade_plan",
+                table: "memberships");
+        }
+    }
+}
