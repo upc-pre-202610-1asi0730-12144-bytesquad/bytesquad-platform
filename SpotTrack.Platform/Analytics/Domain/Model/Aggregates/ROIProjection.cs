ac34d240fbf8c9b66fd7465e0dc66f7788a36b37
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-06 02:32:55 -0500

    fix(analytics): replace Random ID with InitializeId() from EF PK in ROIProjection

--- SpotTrack.Platform/Analytics/Domain/Model/Aggregates/ROIProjection.cs
+++ SpotTrack.Platform/Analytics/Domain/Model/Aggregates/ROIProjection.cs
@@ -1,4 +1,3 @@
-using System;
 using SpotTrack.Platform.Analytics.Domain.Model.Commands;
 using SpotTrack.Platform.Analytics.Domain.Model.ValueObjects;
 
@@ -7,22 +6,23 @@ namespace SpotTrack.Platform.Analytics.Domain.Model.Aggregates;
 public class ROIProjection
 {
     public long Id { get; private set; }
-    public ROIProjectionId RoiProjectionId { get; private set; }
+    public ROIProjectionId RoiProjectionId { get; private set; } = null!;
     public double ProjectedDowntimeCost { get; private set; }
     public double ProjectedEarnings { get; private set; }
     public double RoiIndex { get; private set; }
-    public string DemandStatus { get; private set; }
+    public string DemandStatus { get; private set; } = null!;
 
     protected ROIProjection() { }
 
     public ROIProjection(RequestDowntimeCostProjectionCommand command)
     {
-        RoiProjectionId = new ROIProjectionId(new Random().Next(1000, 100000));
         ProjectedDowntimeCost = command.ProjectedDowntimeCost;
         ProjectedEarnings = 0;
         RoiIndex = 0;
         DemandStatus = "UNDER_REVIEW";
     }
+
+    public void InitializeId() => RoiProjectionId = new ROIProjectionId(Id);
 
     public void UpdateProjectedEarnings(double projectedEarnings)
     {
