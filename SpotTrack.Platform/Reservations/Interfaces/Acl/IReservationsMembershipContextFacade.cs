315532a1d44c01413af87401cc2993c0f5b193ba
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-06 03:34:00 -0500

    refactor(reservations): rename IMembershipContextFacade to IReservationsMembershipContextFacade to avoid ambiguity with Gym BC

--- SpotTrack.Platform/Reservations/Interfaces/Acl/IReservationsMembershipContextFacade.cs
+++ SpotTrack.Platform/Reservations/Interfaces/Acl/IReservationsMembershipContextFacade.cs
@@ -1,6 +1,6 @@
 namespace SpotTrack.Platform.Reservations.Interfaces.Acl;
 
-public interface IMembershipContextFacade
+public interface IReservationsMembershipContextFacade
 {
     Task<bool> GymHasActiveMembershipAsync(int equipmentId, CancellationToken cancellationToken);
 }
