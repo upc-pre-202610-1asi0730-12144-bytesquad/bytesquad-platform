ce26c32ea3079e4755db23275d3fffd5db60cf6e
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-06 18:13:40 -0500

    feat(infrastructure): add AddBusinessesTable migration

---
+++ SpotTrack.Platform/Migrations/20260706231320_AddBusinessesTable.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace SpotTrack.Platform.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBusinessesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "businesses",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    admin_id = table.Column<int>(type: "int", nullable: false),
+                    company_name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    ruc = table.Column<string>(type: "varchar(11)", maxLength: 11, nullable: false),
+                    legal_structure = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    company_phone = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: false),
+                    company_email = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_businesses", x => x.id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "i_x_businesses_admin_id",
+                table: "businesses",
+                column: "admin_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "businesses");
+        }
+    }
+}
