bfc3b84009b9538876a41f56a638e1c4e7e22a87
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-07 08:34:12 -0500

    feat(monitoring): add ISensorRepository

---
+++ SpotTrack.Platform/Monitoring/Domain/Repositories/ISensorRepository.cs
@@ -0,0 +1,10 @@
+using SpotTrack.Platform.Monitoring.Domain.Model.Aggregates;
+using SpotTrack.Platform.Shared.Domain.Repositories;
+
+namespace SpotTrack.Platform.Monitoring.Domain.Repositories;
+
+public interface ISensorRepository : IBaseRepository<Sensor>
+{
+    Task<IEnumerable<Sensor>> FindAllBySensorTypeAsync(SensorType type, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Sensor>> FindAllByAdminIdAndTypeAsync(int adminId, SensorType type, CancellationToken cancellationToken = default);
+}
