8c70dffe5876f96c8431f385d03238ed528a4518
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-06 23:24:56 -0500

    feat(gym): simplify GymResource to { id, name }

--- SpotTrack.Platform/Gym/Interfaces/Rest/Resources/GymResource.cs
+++ SpotTrack.Platform/Gym/Interfaces/Rest/Resources/GymResource.cs
@@ -1,3 +1,3 @@
 namespace SpotTrack.Platform.Gyms.Interfaces.Rest.Resources;
 
-public record GymResource(int Id, string Name, string Street, string District, string City);
+public record GymResource(int Id, string Name);
