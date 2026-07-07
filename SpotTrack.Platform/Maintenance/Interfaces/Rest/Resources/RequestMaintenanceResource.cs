570b09d9c86016f36d7cba4632290379b18b7776
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-07 10:11:44 -0500

    feat(maintenance): add Priority and Type to RequestMaintenanceResource

--- SpotTrack.Platform/Maintenance/Interfaces/Rest/Resources/RequestMaintenanceResource.cs
+++ SpotTrack.Platform/Maintenance/Interfaces/Rest/Resources/RequestMaintenanceResource.cs
@@ -1,3 +1,5 @@
+using SpotTrack.Platform.Maintenances.Domain.Model;
+
 namespace SpotTrack.Platform.Maintenances.Interfaces.Rest.Resources;
 
-public record RequestMaintenanceResource(int EquipmentId, string Reason);
+public record RequestMaintenanceResource(int EquipmentId, string Reason, EMaintenancePriority Priority, EMaintenanceType Type);
