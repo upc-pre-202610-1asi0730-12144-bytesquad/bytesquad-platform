062e18a16339d8d8b66f31d5f702ab6633ae0e1e
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-07 02:45:09 -0500

    fix(reservations): add equipmentId param to ClientGymHasActiveMembershipAsync interface

--- SpotTrack.Platform/Reservations/Interfaces/Acl/IReservationsMembershipContextFacade.cs
+++ SpotTrack.Platform/Reservations/Interfaces/Acl/IReservationsMembershipContextFacade.cs
@@ -3,5 +3,5 @@ namespace SpotTrack.Platform.Reservations.Interfaces.Acl;
 public interface IReservationsMembershipContextFacade
 {
     Task<bool> GymHasActiveMembershipAsync(int equipmentId, CancellationToken cancellationToken);
-    Task<bool> ClientGymHasActiveMembershipAsync(int userId, CancellationToken cancellationToken);
+    Task<bool> ClientGymHasActiveMembershipAsync(int userId, int equipmentId, CancellationToken cancellationToken);
 }
