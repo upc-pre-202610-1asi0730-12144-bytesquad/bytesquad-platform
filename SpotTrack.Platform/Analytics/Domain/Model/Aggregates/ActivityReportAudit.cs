ea6353b1218673104440082c8e384682192ce51f
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-06 21:11:23 -0500

    feat(analytics): add IAuditableEntity to ActivityReport

---
+++ SpotTrack.Platform/Analytics/Domain/Model/Aggregates/ActivityReportAudit.cs
@@ -0,0 +1,10 @@
+using SpotTrack.Platform.Shared.Domain.Model.Entities;
+
+namespace SpotTrack.Platform.Analytics.Domain.Model.Aggregates
+{
+    public partial class ActivityReport : IAuditableEntity
+    {
+        public DateTimeOffset? CreatedAt { get; set; }
+        public DateTimeOffset? UpdatedAt { get; set; }
+    }
+}
