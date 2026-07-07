b1424e340d5906e8d70da8919f76fdcd0aef2be1
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-07 10:31:40 -0500

    fix(maintenance): use string for Priority and Type in RequestMaintenanceResource

--- SpotTrack.Platform/Maintenance/Interfaces/Rest/Resources/RequestMaintenanceResource.cs
+++ SpotTrack.Platform/Maintenance/Interfaces/Rest/Resources/RequestMaintenanceResource.cs
@@ -1,5 +1,3 @@
-using SpotTrack.Platform.Maintenances.Domain.Model;
-
 namespace SpotTrack.Platform.Maintenances.Interfaces.Rest.Resources;
 
-public record RequestMaintenanceResource(int EquipmentId, string Reason, EMaintenancePriority Priority, EMaintenanceType Type);
+public record RequestMaintenanceResource(int EquipmentId, string Reason, string Priority, string Type);
