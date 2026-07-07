aa95adeea5427a9afb9d5f2b20afc5127e986488
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-07 08:38:38 -0500

    feat(monitoring): add MonitoringActionResultAssembler

---
+++ SpotTrack.Platform/Monitoring/Interfaces/Rest/Transform/MonitoringActionResultAssembler.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SpotTrack.Platform.Monitoring.Domain.Model;
+using SpotTrack.Platform.Shared.Application.Model;
+using SpotTrack.Platform.Shared.Interfaces.Rest.ProblemDetails;
+
+namespace SpotTrack.Platform.Monitoring.Interfaces.Rest.Transform;
+
+public static class MonitoringActionResultAssembler
+{
+    private static int MapErrorToStatusCode(Enum error) => error switch
+    {
+        MonitoringError.SensorNotFound or MonitoringError.SessionTrackerNotFound => StatusCodes.Status404NotFound,
+        MonitoringError.InvalidSensorData or MonitoringError.InvalidTrackerData => StatusCodes.Status400BadRequest,
+        _ => StatusCodes.Status500InternalServerError
+    };
+
+    public static IActionResult ToSuccessActionResult<TEntity, TResource>(
+        TEntity entity,
+        Func<TEntity, TResource> toResource,
+        int statusCode,
+        ControllerBase controller) =>
+        controller.StatusCode(statusCode, toResource(entity));
+
+    public static IActionResult ToFailureActionResult<T>(
+        Result<T> result,
+        ControllerBase controller,
+        ProblemDetailsFactory factory) =>
+        factory.CreateProblemDetails(controller, MapErrorToStatusCode(result.Error!), result.Error, result.Message);
+}
