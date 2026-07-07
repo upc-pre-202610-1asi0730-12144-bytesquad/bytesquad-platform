a0c6afb61889cf8a8e6338579bc77e96ec08a8a1
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-06 23:26:26 -0500

    feat(gym): remove unused IGymCommandService dependency from GymContextFacade

--- SpotTrack.Platform/Gym/Application/Acl/GymContextFacade.cs
+++ SpotTrack.Platform/Gym/Application/Acl/GymContextFacade.cs
@@ -1,7 +1,6 @@
 using SpotTrack.Platform.Gyms.Domain.Model.Aggregates;
 using SpotTrack.Platform.Gyms.Domain.Model.Commands;
 using SpotTrack.Platform.Gyms.Domain.Repositories;
-using SpotTrack.Platform.Gyms.Domain.Services;
 using SpotTrack.Platform.Gyms.Interfaces.Acl;
 using SpotTrack.Platform.Shared.Domain.Repositories;
 
@@ -10,7 +9,6 @@ namespace SpotTrack.Platform.Gyms.Application.Acl;
 public class GymContextFacade(
     IEquipmentCommandService equipmentCommandService,
     IGymRepository gymRepository,
-    IGymCommandService gymCommandService,
     IUnitOfWork unitOfWork) : IGymContextFacade
 {
     public async Task<bool> OccupyEquipmentAsync(int equipmentId)
