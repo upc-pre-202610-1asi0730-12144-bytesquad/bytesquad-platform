2ce4ec7227bb7d76139aca124306da37b8bd4979
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-07 09:54:04 -0500

    feat(maintenance): add ExistsByTechnicalTicketIdAsync to IMaintenanceLogRepository

--- SpotTrack.Platform/Maintenance/Domain/Repositories/IMaintenanceLogRepository.cs
+++ SpotTrack.Platform/Maintenance/Domain/Repositories/IMaintenanceLogRepository.cs
@@ -11,4 +11,7 @@ public interface IMaintenanceLogRepository : IBaseRepository<MaintenanceLog>
         CancellationToken cancellationToken = default);
     Task<IEnumerable<MaintenanceLog>> FindAllByTicketIdAsync(int ticketId,
         CancellationToken cancellationToken = default);
+
+    Task<bool> ExistsByTechnicalTicketIdAsync(int ticketId,
+        CancellationToken cancellationToken = default);
 }
