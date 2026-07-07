c4e6c41bc616d9343e6742c3acb32e62b9b63d14
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-07 05:00:54 -0500

    feat(gyms): implement GetGymWithBranchesByIdQuery using FindByIdWithBranchesAndZonesAsync

--- SpotTrack.Platform/Gym/Application/Internal/QueryServices/GymQueryService.cs
+++ SpotTrack.Platform/Gym/Application/Internal/QueryServices/GymQueryService.cs
@@ -15,4 +15,7 @@ public class GymQueryService(IGymRepository gymRepository) : IGymQueryService
 
     public async Task<IEnumerable<Gym>> Handle(GetAllGymsQuery query, CancellationToken cancellationToken)
         => await gymRepository.ListAsync(cancellationToken);
+
+    public async Task<Gym?> Handle(GetGymWithBranchesByIdQuery query, CancellationToken cancellationToken)
+        => await gymRepository.FindByIdWithBranchesAndZonesAsync(query.GymId, cancellationToken);
 }
