bafd54e9a66a9e415b4c675385bee976e3258a9e
ORION-tech-c <asfernandezl.peru@gmail.com>
2026-07-07 02:47:41 -0500

    feat(gyms): update EF model snapshot for AddGymAuthorizedDnis migration

--- SpotTrack.Platform/Migrations/AppDbContextModelSnapshot.cs
+++ SpotTrack.Platform/Migrations/AppDbContextModelSnapshot.cs
@@ -192,6 +192,33 @@ namespace SpotTrack.Platform.Migrations
                     b.ToTable("gyms");
                 });
 
+            modelBuilder.Entity("SpotTrack.Platform.Gyms.Domain.Model.Entities.AuthorizedDni", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int")
+                        .HasColumnName("id");
+
+                    b.Property<string>("Dni")
+                        .IsRequired()
+                        .HasMaxLength(8)
+                        .HasColumnType("varchar(8)")
+                        .HasColumnName("dni");
+
+                    b.Property<int>("GymId")
+                        .HasColumnType("int")
+                        .HasColumnName("gym_id");
+
+                    b.HasKey("Id")
+                        .HasName("p_k_gym_authorized_dnis");
+
+                    b.HasIndex("GymId", "Dni")
+                        .IsUnique()
+                        .HasDatabaseName("i_x_gym_authorized_dnis_gym_id_dni");
+
+                    b.ToTable("gym_authorized_dnis", (string)null);
+                });
+
             modelBuilder.Entity("SpotTrack.Platform.Gyms.Domain.Model.Entities.Branch", b =>
                 {
                     b.Property<int>("Id")
